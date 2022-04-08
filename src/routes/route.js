63f5e5a6f3541eeda8deb7247eace59645bbdcc7
Sumit23199702 <102967839+Sumit23199702@users.noreply.github.com>
2022-04-08 23:21:42 +0530

    file name

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,22 +3,61 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/user-profile/:abcd', function(req, res) {
-    console.log(req)
-    console.log(req.params.abcd)
-    res.send('dummy response')
-})
-
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+// problem 1
+router.get('/movies', function (req, res) {
+    let arr = ["Rang De Basanti", "The Shining", "Lord Of The Rings", "Batman Begins"]
+    res.send(arr)
 });
 
+// Problem 2 & 3 
 
+router.get('/movies/:indexNumber', function (req, res) {
+    let arr1 = ["Rang De Basanti", "The Shining", "Lord Of The Rings", "Batman Begins"]
+    let indexNumber = req.params.indexNumber
+    if (indexNumber > arr1.length - 1) {
+        res.send("Value is above than index number")
+    }
+    else {
+        res.send(arr1[indexNumber])
+    }
+});
 
+// Problem 4
+
+router.get('/films', function (req, res) {
+    let pqrs = [{ id: 1, name: "The Shining" }, { id: 2, name: "Incendies" }, { id: 3, name: "Rang de Basanti" }, { id: 4, name: "Finding Nemo" }]
+
+    res.send(pqrs)
+});
+
+// Problem 5
+
+router.get('/film/:filmid', function (req, res) {
+    const Fnames = [{
+        'id': 1,
+        'name': 'The Shining'
+    }, {
+        'id': 2,
+        'name': 'Incendies'
+    }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+    }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+    }]
+    let a;
+    let n = req.params.filmid
+    for (let i = 0; i < Fnames.length; i++) {
+        if(n>Fnames[i].id){
+            a="No movie exists with this id"
+        }
+        if(n== Fnames[i].id){
+            a= Fnames[i]
+        }
+    }
+    res.send(a)
+});
 
 module.exports = router;
 // adding this comment for no reason
